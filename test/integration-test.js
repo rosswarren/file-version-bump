495208cad3d940cb4cd6a630d4f34f4e3c0c00e9
rosswarren <rosswarren4@gmail.com>
2016-07-10 15:59:55 +0000

    update dependencies and remove some custom linting rules

--- test/integration-test.js
+++ test/integration-test.js
@@ -6,11 +6,11 @@ const rimraf = P.promisify(require('rimraf'));
 const exec = P.promisify(require('child_process').exec);
 
 describe('integration', function() {
-    this.timeout(20000);
+  this.timeout(20000);
 
-    it('shows a message if no config present', () =>
+  it('shows a message if no config present', () =>
         exec('npm link', {
-            cwd: process.cwd()
+          cwd: process.cwd()
         })
         .then(() => rimraf('./bumptest'))
         .then(() => P.promisify(fs.mkdir)('./bumptest'))
@@ -31,24 +31,24 @@ describe('integration', function() {
 `
         ))
         .then(() => exec('npm link file-version-bump', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(() => exec('npm version minor', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(stdout => {
-            assert(stdout.indexOf('No file-version-bump config') !== -1);
+          assert(stdout.indexOf('No file-version-bump config') !== -1);
         })
     );
 
-    it('bumps the files specified in the package.json', () =>
+  it('bumps the files specified in the package.json', () =>
         exec('npm link', {
-            cwd: process.cwd()
+          cwd: process.cwd()
         })
         .then(() => rimraf('./bumptest'))
         .then(() => P.promisify(fs.mkdir)('./bumptest'))
         .then(() => exec('git init', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(() => P.promisify(fs.writeFile)('./bumptest/package.json',
 `
@@ -91,40 +91,40 @@ A repository for testing the file-version-bump repo v0.3.5 and 1.2.3
 `
         ))
         .then(() => exec('git add .', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(() => exec('git commit -m "first"', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(() => exec('npm link file-version-bump', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then(() => exec('npm version minor', {
-            cwd: path.join(process.cwd(), 'bumptest')
+          cwd: path.join(process.cwd(), 'bumptest')
         }))
         .then((stdout, stderr) => {
-            console.error(stderr);
+          console.error(stderr);
 
-            assert.deepEqual(stdout.split('\n').slice(0, 1), [
-                'v1.1.0'
-            ]);
+          assert.deepEqual(stdout.split('\n').slice(0, 1), [
+            'v1.1.0'
+          ]);
 
-            assert.deepEqual(stdout.split('\n').slice(3), [
-                '> file-version-bump',
-                '',
-                '- Glob is README.md',
-                'found file README.md',
-                '0.3.5 ==> 1.1.0',
-                '1.2.3 ==> 1.1.0',
-                '8.8.2 ==> 1.1.0',
-                '- Glob is doesnotexist',
-                '- Glob is src/**/*.{js,json}',
-                'found file src/something.json',
-                'found file src/version.js',
-                '1.0.0 ==> 1.1.0',
-                '0.2.3 ==> 1.1.0',
-                ''
-            ]);
+          assert.deepEqual(stdout.split('\n').slice(3), [
+            '> file-version-bump',
+            '',
+            '- Glob is README.md',
+            'found file README.md',
+            '0.3.5 ==> 1.1.0',
+            '1.2.3 ==> 1.1.0',
+            '8.8.2 ==> 1.1.0',
+            '- Glob is doesnotexist',
+            '- Glob is src/**/*.{js,json}',
+            'found file src/something.json',
+            'found file src/version.js',
+            '1.0.0 ==> 1.1.0',
+            '0.2.3 ==> 1.1.0',
+            ''
+          ]);
         })
     );
 });
